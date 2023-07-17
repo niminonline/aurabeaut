cffff5d1677f00906e8f601c064e7c010bfafabf
niminonline <niminonline@gmail.com>
2023-07-17 12:59:52 +0530

    Hosting alpha

--- mongodb/mongoConnect.js
+++ mongodb/mongoConnect.js
@@ -1,16 +1,54 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const mongoose = require('mongoose');
 dotenv.config();
-const connectDB = () => {
-  try {
-    mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.mongo_url);
-  } catch (error) {
-    console.log('connected');
-  }
-};
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-module.exports = {
-  connectDB
-};
+
+//==============MongoDb Compass=====================
+
+// const connectDB = () => {
+//   try {
+//     mongoose.set('strictQuery', true);
+//     mongoose.connect(process.env.mongo_url);
+//   } catch (error) {
+//     console.log('connected');
+//   }
+// };
+
+// module.exports = {
+//   connectDB
+// };
+
+
+//==============MongoDb Atlas=====================
+
+
+
+
+const uri =process.env.mongo_url;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
